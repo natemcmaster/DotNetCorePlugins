8404831d95a767ab7ca81fcf6eb8eefcf25f4bf9
Nate McMaster <natemcmaster@users.noreply.github.com>
2018-09-04 22:30:52 -0700

    Add API to configure the loader to prefer shared types by default (#8)

---
+++ src/Plugins/PluginLoaderOptions.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) Nate McMaster.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+namespace McMaster.NETCore.Plugins
+{
+    /// <summary>
+    /// Options for how <see cref="PluginLoader"/> behaves.
+    /// </summary>
+    public enum PluginLoaderOptions
+    {
+        /// <summary>
+        /// Use the default behavior.
+        /// </summary>
+        None,
+
+        /// <summary>
+        /// Attempt to unify all types from a plugin with the host.
+        /// <para>
+        /// This does not guarantee types will unify.
+        /// </para>
+        /// </summary>
+        PreferSharedTypes,
+    }
+}
