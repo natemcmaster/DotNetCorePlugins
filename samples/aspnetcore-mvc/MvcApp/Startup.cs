a641e86c21879346e3043fed4a1544b971b293fd
Nate McMaster <nate.mcmaster@gmail.com>
2019-07-15 21:31:53 -0700

    feat: add a new package, Plugins.Mvc, for loading MVC controllers and Razor views from a plugin

--- samples/aspnetcore-mvc/MvcApp/Startup.cs
+++ samples/aspnetcore-mvc/MvcApp/Startup.cs
@@ -1,10 +1,7 @@
 ﻿using System;
 using System.IO;
-using System.Reflection;
-using McMaster.NETCore.Plugins;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.ApplicationParts;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace MvcWebApp
@@ -19,37 +16,9 @@ namespace MvcWebApp
 
             foreach (var dir in Directory.GetDirectories(Path.Combine(AppContext.BaseDirectory, "plugins")))
             {
-                var pluginName = Path.GetFileName(dir);
-
-                var plugin = PluginLoader.CreateFromAssemblyFile(
-                    Path.Combine(dir, pluginName + ".dll"), // create a plugin from for the .dll file
-                    config =>
-                        // this ensures that the version of MVC is shared between this app and the plugin
-                        config.PreferSharedTypes = true);
-
-                var pluginAssembly = plugin.LoadDefaultAssembly();
-                Console.WriteLine($"Loading application parts from plugin {pluginName}");
-
-                // This loads MVC application parts from plugin assemblies
-                var partFactory = ApplicationPartFactory.GetApplicationPartFactory(pluginAssembly);
-                foreach (var part in partFactory.GetApplicationParts(pluginAssembly))
-                {
-                    Console.WriteLine($"* {part.Name}");
-                    mvcBuilder.PartManager.ApplicationParts.Add(part);
-                }
-
-                // This piece finds and loads related parts, such as MvcAppPlugin1.Views.dll.
-                var relatedAssembliesAttrs = pluginAssembly.GetCustomAttributes<RelatedAssemblyAttribute>();
-                foreach (var attr in relatedAssembliesAttrs)
-                {
-                    var assembly = plugin.LoadAssembly(attr.AssemblyFileName);
-                    partFactory = ApplicationPartFactory.GetApplicationPartFactory(assembly);
-                    foreach (var part in partFactory.GetApplicationParts(assembly))
-                    {
-                        Console.WriteLine($"  * {part.Name}");
-                        mvcBuilder.PartManager.ApplicationParts.Add(part);
-                    }
-                }
+                var pluginFile = Path.Combine(dir, Path.GetFileName(dir) + ".dll");
+                // The AddPluginFromAssemblyFile method comes from McMaster.NETCore.Plugins.Mvc
+                mvcBuilder.AddPluginFromAssemblyFile(pluginFile);
             }
         }
 
