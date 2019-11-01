54942bec5938369278edee87b0c463d4be8e20d5
Nate McMaster <nate.mcmaster@gmail.com>
2019-10-31 22:06:59 -0700

    feature: add IMvcBuilder.AddPluginLoader

--- src/Plugins.Mvc/MvcPluginExtensions.cs
+++ src/Plugins.Mvc/MvcPluginExtensions.cs
@@ -13,7 +13,11 @@ namespace Microsoft.Extensions.DependencyInjection
     public static class MvcPluginExtensions
     {
         /// <summary>
-        /// Loads
+        /// Loads controllers and razor pages from a plugin assembly.
+        /// <para>
+        /// This creates a loader with <see cref="PluginConfig.PreferSharedTypes" /> set to <c>true</c>.
+        /// If you need more control over shared types, use <see cref="AddPluginLoader" /> instead.
+        /// </para>
         /// </summary>
         /// <param name="mvcBuilder">The MVC builder</param>
         /// <param name="assemblyFile">Full path the main .dll file for the plugin.</param>
@@ -26,7 +30,24 @@ namespace Microsoft.Extensions.DependencyInjection
                     // this ensures that the version of MVC is shared between this app and the plugin
                     config.PreferSharedTypes = true);
 
-            var pluginAssembly = plugin.LoadDefaultAssembly();
+            return mvcBuilder.AddPluginLoader(plugin);
+        }
+
+        /// <summary>
+        /// Loads controllers and razor pages from a plugin loader.
+        /// <para>
+        /// In order for this to work, the PluginLoader instance must be configured to share the types
+        /// <see cref="ProvideApplicationPartFactoryAttribute" /> and <see cref="RelatedAssemblyAttribute" />
+        /// (comes from Microsoft.AspNetCore.Mvc.Core.dll). The easiest way to ensure that is done correctly
+        /// is to set <see cref="PluginConfig.PreferSharedTypes" /> to <c>true</c>.
+        /// </para>
+        /// </summary>
+        /// <param name="mvcBuilder">The MVC builder</param>
+        /// <param name="pluginLoader">An instance of PluginLoader.</param>
+        /// <returns>The builder</returns>
+        public static IMvcBuilder AddPluginLoader(this IMvcBuilder mvcBuilder, PluginLoader pluginLoader)
+        {
+            var pluginAssembly = pluginLoader.LoadDefaultAssembly();
 
             // This loads MVC application parts from plugin assemblies
             var partFactory = ApplicationPartFactory.GetApplicationPartFactory(pluginAssembly);
@@ -39,7 +60,7 @@ namespace Microsoft.Extensions.DependencyInjection
             var relatedAssembliesAttrs = pluginAssembly.GetCustomAttributes<RelatedAssemblyAttribute>();
             foreach (var attr in relatedAssembliesAttrs)
             {
-                var assembly = plugin.LoadAssembly(attr.AssemblyFileName);
+                var assembly = pluginLoader.LoadAssembly(attr.AssemblyFileName);
                 partFactory = ApplicationPartFactory.GetApplicationPartFactory(assembly);
                 foreach (var part in partFactory.GetApplicationParts(assembly))
                 {
