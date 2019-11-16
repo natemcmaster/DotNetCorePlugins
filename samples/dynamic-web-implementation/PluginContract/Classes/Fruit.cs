05fc0debf9efb20f18f25d6cf1d4ed553c33f2b7
Emanuele Bucarelli <e.bucarelli@sintraconsulting.eu>
2019-11-16 09:52:34 +0100

    rename plugin contract project

---
+++ samples/dynamic-web-implementation/PluginContract/Classes/Fruit.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PluginLib.Classes
+{
+    public class Fruit
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ samples/dynamic-web-implementation/PluginContract/FruitController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PluginLib.Classes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PluginLib
+{
+    [Route("api/fruit")]
+    [ApiController]
+    public class FruitController: ControllerBase
+    {
+        private readonly IFruitService _fruitService;
+        public FruitController(IFruitService service)
+        {
+           _fruitService = service;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Fruit>> Get()
+        {
+            return _fruitService.GetFruits();
+        }
+    }
+}
