3f2086285740525846b4385d76369fe32a51f710
Nate McMaster <nate.mcmaster@gmail.com>
2019-12-08 11:58:10 -0800

    Add code comments to hello-world sample
    Addresses #103

--- samples/hello-world/PluginContract/IPlugin.cs
+++ samples/hello-world/PluginContract/IPlugin.cs
@@ -1,5 +1,11 @@
 namespace HelloWorld
 {
+    /// <summary>
+    /// This interface is an example of one way to define the interactions between the host and plugins.
+    /// There is nothing special about the name "IPlugin"; it's used here to illustrate a concept.
+    /// Look at https://github.com/natemcmaster/DotNetCorePlugins/tree/master/samples for additional examples
+    /// of ways you could define the interaction between host and plugins.
+    /// </summary>
     public interface IPlugin
     {
         string GetName();
