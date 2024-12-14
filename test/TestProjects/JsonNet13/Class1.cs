b4f2e1111d32d4ccaef0bfc4bb1e1a368ab8300e
Nate McMaster <nate.mcmaster@gmail.com>
2024-12-14 11:40:24 -0800

    cleanup: remove dependency on 3rd party lib for testing private dependencies

--- test/TestProjects/JsonNet13/Class1.cs
+++
@@ -1,9 +0,0 @@
-﻿// Copyright (c) Nate McMaster.
-// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
-
-namespace JsonNet13
-{
-    public class Class1
-    {
-    }
-}
