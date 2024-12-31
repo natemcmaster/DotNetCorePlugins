dca8513be81c08638dc07bc39ca39588148661e6
Nate McMaster <nate.mcmaster@gmail.com>
2024-12-30 21:06:06 -0800

    update release notes for 2.0 release (#299)

--- build.ps1
+++ build.ps1
@@ -20,14 +20,7 @@ $artifacts = "$PSScriptRoot/artifacts/"
 
 Remove-Item -Recurse $artifacts -ErrorAction Ignore
 
-exec dotnet tool restore
-
-[string[]] $formatArgs=@()
-if ($ci) {
-    $formatArgs += '--check'
-}
-
-exec dotnet tool run dotnet-format -- -v detailed @formatArgs
+exec dotnet format
 exec dotnet build --configuration $Configuration '-warnaserror:CS1591'
 exec dotnet pack --no-restore --no-build --configuration $Configuration -o $artifacts
 exec dotnet test --no-restore --no-build --configuration $Configuration `
